e0139ce45ebeee2ef526246ed070d65d07b95a35
Jorge J. Caraballo <jcaraballo79@gmail.com>
2025-12-18 16:25:32 -0500

    Update from Chapter 1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageName = ""
+    @State private var imageName = "image0"
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     
     var body: some View {
         
@@ -28,14 +29,24 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Show Message") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
                 
-                message = (message == message1 ? message2 : message1)
+                let messages = ["You are Awesome!",
+                                "You are Great!",
+                                "You are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
                 
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
