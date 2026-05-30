83a2916c60cdbea9195dfa0b1c0ee7083ff33030
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-05-29 22:51:32 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Jorge Caraballo on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
